089c9ec400cab463cb2bae149016e5349e2c5e7a
Valeria <valtpocketbook@gmail.com>
2024-09-22 23:01:38 +0200

    recycler + Timer

--- app/src/main/java/com/valeriia/beta_ver_1/HomeFragment.java
+++ app/src/main/java/com/valeriia/beta_ver_1/HomeFragment.java
@@ -25,23 +25,43 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Создаем список статей
+        // Создаем список статей для food
         List<Article> articleList = new ArrayList<>();
         articleList.add(new Article(1, "ABC", "img01", "Food"));
-        articleList.add(new Article(2, "DEF", "img01", "Food"));
-        articleList.add(new Article(3, "GHM", "img01", "Food"));
-        articleList.add(new Article(4, "WAW", "img01", "Food"));
+        articleList.add(new Article(2, "DEF", "img02", "Food"));
+        articleList.add(new Article(3, "GHM", "img03", "Food"));
+        articleList.add(new Article(4, "WAW", "img04", "Food"));
+
+        // Создаем список статей для healthcare
+        List<Article> articleList2 = new ArrayList<>();
+        articleList2.add(new Article(1, "ABC", "img03", "Healthcare"));
+        articleList2.add(new Article(2, "DEF", "img01", "Healthcare"));
+        articleList2.add(new Article(3, "GHM", "img04", "Healthcare"));
+        articleList2.add(new Article(4, "WAW", "img01", "Healthcare"));
 
         // Настраиваем RecyclerView
         setArticleRecycler(view, articleList);
+        setArticleRecycler2(view, articleList2);
 
+        // TODO: метод один setArticleRecycler сделать, сделать переменную для темы в конструкторе, сделать один список с ифом темы
         return view;
     }
 
     private void setArticleRecycler(View view, List<Article> articlesList) {
         // Инициализация RecyclerView
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false);
-        articleRecycler = view.findViewById(R.id.articleRecycler);  // Найти RecyclerView внутри фрагмента
+        articleRecycler = view.findViewById(R.id.foodArticleRecycler);  // Найти RecyclerView внутри фрагмента
+        articleRecycler.setLayoutManager(layoutManager);
+
+        // Установка адаптера для RecyclerView
+        articleAdapter = new ArticleAdapter(getContext(), articlesList);
+        articleRecycler.setAdapter(articleAdapter);
+    }
+
+    private void setArticleRecycler2(View view, List<Article> articlesList) {
+        // Инициализация RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false);
+        articleRecycler = view.findViewById(R.id.healthcareArticleRecycler);  // Найти RecyclerView внутри фрагмента
         articleRecycler.setLayoutManager(layoutManager);
 
         // Установка адаптера для RecyclerView
--- app/src/main/java/com/valeriia/beta_ver_1/TimerFragment.java
+++ app/src/main/java/com/valeriia/beta_ver_1/TimerFragment.java
@@ -1,20 +1,135 @@
 package com.valeriia.beta_ver_1;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimerFragment extends Fragment {
+
+    private TextView timerText;
+    private Button stopStartButton;
+
+    private Timer timer;
+    private TimerTask timerTask;
+    private Double time = 0.0;
+
+    private boolean timerStarted = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        timerText = view.findViewById(R.id.timerText);
+        stopStartButton = view.findViewById(R.id.startStopButton);
+
+        timer = new Timer();
+
+        // Set listeners for buttons (if necessary)
+        stopStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStopTapped(v);
+            }
+        });
+
+        view.findViewById(R.id.resetButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTapped(v);
+            }
+        });
+
+        return view;
+    }
+
+    public void resetTapped(View view) {
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(requireContext());
+        resetAlert.setTitle("Reset Timer");
+        resetAlert.setMessage("Are you sure you want to reset the timer?");
+        resetAlert.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (timerTask != null) {
+                    timerTask.cancel();
+                    setButtonUI("START", R.color.black);
+                    time = 0.0;
+                    timerStarted = false;
+                    timerText.setText(formatTime(0, 0, 0));
+                }
+            }
+        });
+
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Do nothing
+            }
+        });
+
+        resetAlert.show();
+    }
+
+    public void startStopTapped(View view) {
+        if (!timerStarted) {
+            timerStarted = true;
+            setButtonUI("STOP", R.color.black);
+            startTimer();
+        } else {
+            timerStarted = false;
+            setButtonUI("START", R.color.white);
+
+            if (timerTask != null) {
+                timerTask.cancel();
+            }
+        }
+    }
+
+    private void setButtonUI(String text, int colorResId) {
+        stopStartButton.setText(text);
+        stopStartButton.setTextColor(ContextCompat.getColor(requireContext(), colorResId));
+    }
+
+    private void startTimer() {
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                requireActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time++;
+                        timerText.setText(getTimerText());
+                    }
+                });
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
+    }
+
+    private String getTimerText() {
+        int rounded = (int) Math.round(time);
+
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours) {
+        return String.format("%02d", hours) + " : " + String.format("%02d", minutes) + " : " + String.format("%02d", seconds);
     }
 }
